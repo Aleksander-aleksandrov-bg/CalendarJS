c0da6624dfef90457ac6566de85baeb18e8221e1
Aleksander Aleksandrov <aleksaander_aleksandrov@abv.bg>
2013-12-07 10:59:59 +0200

    Uploading calendar
    Pushing the final app to github

---
+++ Calendar/Calendar/CalendarLibrary - Copy.js
@@ -0,0 +1,80 @@
+﻿var Toolbar = (function () {
+
+
+    var Menu = function (options) {  //class
+
+        _this = this; // kogato izpolzvame this s dolna cherta sochi kym scope na nashiqt model
+
+        this.parent = document.querySelector(options.parent); //vzimame id
+        this.items = options.items; // vzimame kopie na masiva s elementi
+        this.elements = []; // dopalnitelen masiv, pazi instanciite na nashite itemi
+        this.type = options.type;
+
+        this.addElements(this.items,this.type); //dobavqme elementite
+        this.render(this.elements); //izobrazqvame elmentite
+
+        this.parent.addEventListener('click', function (e) {
+
+            for (var index = 0; index < _this.parent.children.length; index++) {
+                el = _this.parent.children[index];
+                el.className = el.className.replace("active", "");
+            }
+            e.target.className = "active";
+        }, false)
+
+    };
+
+
+    Menu.prototype.addElements = function (elements,type) {
+        if (isArray(elements)) {
+            for (var index = 0, len = elements.length; index < len; index += 1) // obrabotavame masiva
+            {
+                this.elements.push(new Item(elements[index], type)); // vkarvame nov obekt v elements masiva
+            }
+        }
+        else {
+            this.elements.push(new Item(elements, type));
+        }
+    };
+    function isArray(object) {
+        if (object.constructor === Array) return true;
+        else return false;
+    }
+
+    Menu.prototype.render = function (elements,type) {
+
+        var builder = "";
+        for (var index = 0, len = elements.length; index < len; index += 1) {
+            builder += elements[index].render(); // pylnim si buildera sys stoinosti <li>
+
+        }
+
+        this.parent.innerHTML = builder; //izobrazqvame na ekrana
+    }; // izobrazqvame na ekrana
+
+
+    var Item = function (name,type) { //class
+        this.name = name;
+        this.type = type;
+    };
+
+
+    Item.prototype.render = function () {
+        
+        return "<" + this.type + ">" + this.name + "</li>"
+    }  //vseki pput kato go viknem vrushta HTML ekvivalenta na nashiqt obekt
+
+
+    return {
+
+        create: function (options) {
+            return new Menu(options);
+            //console.log("It works"); // testvane dali raboti
+        } 								// vryshtame anonimna funkciq s options obekt i vrushta instanciq na obekt MENU
+        // revealing model pattern
+    }
+
+})(); 									//anonymous function, imeadiately invoking anonymous function
+
+
+ // podavame obekt pylen s properties
---
+++ Calendar/Calendar/Library.js
@@ -0,0 +1,87 @@
+﻿var myLibrary = {
+
+    GetDomElement: function (element) {
+        var takenElement = document.querySelectorAll(element);
+        for (var i = 0; i < takenElement.length; i++) {
+            var el = takenElement[i];
+        }
+        return el;
+    },
+
+    AddElement: function (parrent, element, type, obj) {
+        var older = myLibrary.GetDomElement(parrent);
+        var builder = "";
+        if (myLibrary.isArray(element)) {
+            for (var index = 0, len = element.length; index < len; index += 1) // obrabotavame masiva
+            {
+                builder += myLibrary.Make(element[index], type); // vkarvame nov obekt v elements masiva
+            }
+        }
+        else {
+            builder += myLibrary.Make(element, type);
+            var el = myLibrary.Make(element, type);
+            //el.className = "event";
+            if (obj != "undefined") {                
+                for (var i in obj) {
+                    console.log(i);
+                    el[i] = obj[i];
+                }
+            }
+        }
+        older.innerHTML += builder;
+    },
+    isArray: function (object) {
+        if (object.constructor === Array) return true;
+        else return false;
+    },
+    Make: function (name, type) {
+        return "<" + type + ">" + name + "</" + type + ">"
+    },
+    RemoveElement: function (element, child) {
+        if (child[0] == "#" || child[0] == ".") {
+            var child = myLibrary.GetDomElement(child);
+            var parenT = myLibrary.GetDomElement(element);
+            child.parentNode.removeChild(child);
+        }
+        else {
+            var parenT = myLibrary.GetDomElement(element).childNodes;
+            for (var i = 0; i < parenT.length; i++) {
+                if (parenT[i].nodeName.toLowerCase() == child) {
+                    console.log(parenT[i]);
+                    parenT[i].parentNode.removeChild(parenT[i]);
+                }
+            }
+        }
+    },
+
+    ManipulateElementAttribute: function (parrent, element, attribute, name) {
+        if (element[0] == "#" || element[0] == ".") {
+            var elementChange = myLibrary.GetDomElement(element);
+            elementChange.setAttribute(attribute, name);
+            //change(attribute, elementChangee, name);
+        }
+        else {
+            var elementChange = myLibrary.GetDomElement(parrent).childNodes;
+            for (var i = 0; i < elementChange.length; i++) {
+
+                if (elementChange[i].nodeName.toLowerCase() == element) {
+                    console.log("yes");
+                    elementChange[i].setAttribute(attribute, name);
+                    //change(attribute, elementChange[i], name);
+                }
+            }
+        }
+    },
+
+    ManipulateElementTextAndHtml: function (element, content) {
+
+        myLibrary.GetDomElement(element).innerHTML = content;
+    },
+
+    css: function (selector, property, value) {
+        var elem = myLibrary.GetDomElement(selector);
+        console.log(elem);
+        elem.setAttribute("style", property + ":" + value);
+        //}
+    }
+}
